c4da4f54d120bfd51713bcd90dbbb07a0f43c268
SD-github21 <sapandonde.gmail.com>
2022-06-20 23:45:02 -0500

    add user routes and connect to server and db

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
        .then(dbUserData => res.json(dbUserData))
        .catch(err => {
         console.log(err);
@@ -16,6 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     // Access our User model and find one user
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
@@ -49,7 +52,7 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    User.update({
+    User.update(req.body, {
         where: {
             id: req.params.id
         }
