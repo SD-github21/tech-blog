6d01507807cf7176a82042f05b6d5abd2bebc48c
SD-github21 <sapandonde.gmail.com>
2022-06-28 00:14:27 -0500

    added stylesheet, styling, & data variables inside handlebars templates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', {
+        id: 4,
+        title: 'This is my first awesome post',
+        contents: 'This is my first awesome post! I am user ID 4 and I just created my first post on this website!',
+        created_at: '2022-06-26T04:31:48.000Z',
+        comments: [],
+        user: {
+            username: 'test4'
+        }
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -14,6 +15,7 @@ app.set('view engine', 'handlebars');
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
