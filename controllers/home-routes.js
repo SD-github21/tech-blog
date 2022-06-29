9e4b9e3bb8bfbf823169e4d67e6f7dd7a2047e74
SD-github21 <sapandonde.gmail.com>
2022-06-28 20:54:43 -0500

    created signup form and revised login form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,4 +35,11 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
 module.exports = router;
